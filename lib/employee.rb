dd800f08c8e58ab10b77e775eaeae313a104a6aa
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-07 18:16:31 -0500

    add employee validation

--- lib/employee.rb
+++ lib/employee.rb
@@ -5,5 +5,5 @@ class Employee < ActiveRecord::Base
   validates :last_name, presence: true
   validates :hourly_rate, numericality: {greater_than_or_equal_to: 40, less_than_or_equal_to: 200} 
   validates :store, presence: true
-  end
+  
 end
