55106744a7c934110684027d663f7694376e1080
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-07 17:27:51 -0500

    link method to validation

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,6 +1,8 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
 
+  validate :first_name_cannot_be_empty, :last_name_cannot_be_empty, :hourly_rate_cannot_be_outside_range, :must_be_assigned_store
+
   def first_name_cannot_be_empty
     if first_name == ""
       errors.add(:first_name, "Can't be an empty string")
