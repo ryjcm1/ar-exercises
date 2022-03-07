1c29bff59f63a131c6a4349c326e2f0461449c6e
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-07 16:57:47 -0500

    add calculation function

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+@average_revenue = @total_revenue/ Store.count
+@over_one_million = Store.where("annual_revenue > :income", income: 1000000).count
+
+puts "The company's total revenue is: #{@total_revenue}"
+puts "The average revenue of all the stores is: #{@average_revenue}"
+puts "The number of stores earing more than one millions dollars annually is: #{@over_one_million}"
