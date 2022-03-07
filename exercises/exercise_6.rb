fb6be18edf40bd300e09cb327d1f24456f389985
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-07 17:05:17 -0500

    add new employees in store 1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 70)
+@store1.employees.create(first_name: "Carrie", last_name: "Bridgeport", hourly_rate: 80)
+
+@store2.employees.create(first_name: "Able", last_name: "Ham", hourly_rate: 55)
+@store2.employees.create(first_name: "Miranda", last_name: "Remainder", hourly_rate: 65)
+@store2.employees.create(first_name: "Marie", last_name: "Oakly", hourly_rate: 75)
